39cc713119100fc6c2b7305cce3c9ef58d2a6463
sammi-kitty <samantha.agren@icloud.com>
2025-08-25 08:24:23 +0200

    added photos page, needs work with adding photo db functionality

--- routes/main/photos.js
+++ routes/main/photos.js
@@ -1,5 +1,6 @@
 import express from "express"
 import loginRouter from "/login.js"
+import db from "../db-sqlite.js"
 
 const router = express.Router()
 
@@ -7,6 +8,22 @@ app.use("/login", loginRouter)
 
 router.get("/", async (request, response) => {
     response.render("index.njk")
+})
+
+router.get("/page/:pageID", async (request, response) => {
+    const pageID = request.params.pageID
+
+    //const page = Get images from DB or elsewhere using pageID
+
+    const username = await db.get(`
+        SELECT user.name FROM users
+        WHERE user.id = ? LIMIT 1
+        `, pageID)
+
+    response.render("page.njk", {
+        images: images,
+        title: username,
+    })
 })
 
 export default router;
